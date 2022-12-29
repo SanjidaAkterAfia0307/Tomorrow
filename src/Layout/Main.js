6466111270fd4b68427578b421e4d27250ccfaeb
Afia <sanjidaakterafia0307@gmail,com>
2022-12-29 19:11:35 +0600

    finish

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,6 +1,7 @@
-import { Footer } from 'flowbite-react';
+
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer';
 import Header from '../Pages/Shared/Header';
 
 const Main = () => {
